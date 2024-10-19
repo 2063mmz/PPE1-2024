cd8706d6a6cfedb94821b95b883fae2f0a76e27f
YUEEEc <cemmily90@163.com>
2024-10-19 15:46:15 +0200

    Scripts Bash_exercice4

--- exercices/comptes.sh
+++ exercices/comptes.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+N=0
 
-loca1=$(grep "Location" ~/Desktop/Exercicel1/ann/2016/*.ann | wc -l) 
-loca2=$(grep "Location" ~/Desktop/Exercicel1/ann/2017/*.ann | wc -l)
-loca3=$(grep "Location" ~/Desktop/Exercicel1/ann/2018/*.ann | wc -l)
+for an in 2016 2017 2018
+
+do 
 
 
-{echo -e "Location en 2016:\n$loca1"
-echo -e "Location en 2017:\n$loca2"
-echo -e "Location en 2018:\n$loca3"} >> ~/Desktop/Exercicel1/33comptes.txt
+local=$(grep "Location" ~/Desktop/Exercicel1/ann/$an/*.ann | wc -l) 
 
+
+echo -e "Location en $an:\n$local" 
+
+done
