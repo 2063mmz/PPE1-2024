380edbc9c9266605a153b1afe5c2b085aca9305f
YUEEEc <cemmily90@163.com>
2024-10-19 14:37:30 +0200

    ajouter compte_lieux.sh, exercice2

--- exercices/compte_lieux.sh
+++ exercices/compte_lieux.sh
@@ -4,9 +4,9 @@ annee=$1
 mois=$2
 nombre=$3
 
-if [[ $annee =~ ^[0-9]{4}$ ]] && [[ $mois =~ ^(0[1-9]|1[0-2])$ ]] && [[$nombre =~ ^[0-9][0-9]?$ ]]; then
+if [[ $annee =~ ^[0-9]{4}$ ]] && [[ $mois =~ ^(0[1-9]|1[0-2])$ ]] && [[ $nombre =~ ^[0-9][0-9]?$ ]];then
 
-	lieux=$(cat ~/Desktop/Exercicel1/ann/"$annee"/"$annee"_"$mois"_??-???.ann | grep Location | cut -f 3 | uniq | tail -n "$nombre" )
+	lieux=$(grep Location ~/Desktop/Exercicel1/ann/"$annee"/"$annee"_"$mois"_??-???.ann | cut -f 3 | uniq | tail -n "$nombre"| grep "\." )
 	echo "$lieux" >> ~/Desktop/Exercicel1/33lieux.txt
 else
 	echo "ERREUR"
