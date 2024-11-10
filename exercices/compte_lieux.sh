2d50d0a2a9ea3f714beb4df0a7c899bb52219f94
YUEEEc <cemmily90@163.com>
2024-11-10 14:36:00 +0100

    modifier qulquechouse

--- exercices/compte_lieux.sh
+++ exercices/compte_lieux.sh
@@ -14,7 +14,7 @@ nombre=$3
 if [[ $annee =~ ^[0-9]{4}$ ]] && [[ $mois =~ ^(0[1-9]|1[0-2])$ ]] && [[ $nombre =~ ^[0-9][0-9]?$ ]];then
 
 	lieux=$(grep Location ~/Desktop/Exercicel1/ann/"$annee"/"$annee"_"$mois"_??-???.ann | cut -f 3 | uniq | tail -n "$nombre"| grep "\." )
-	echo "$lieux" >> ~/Desktop/Exercicel1/33lieux.txt
+	echo "$lieux" 
 else
 	echo "ERREUR"
 fi
