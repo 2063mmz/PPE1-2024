a1d560284efd13e29826fdc179514c3426fdee84
YUEEEc <cemmily90@163.com>
2024-11-17 04:05:32 +0100

    modifier les erreurs

--- miniprojet/programmes/miniprojet.sh
+++ miniprojet/programmes/miniprojet.sh
@@ -27,26 +27,28 @@ for url in "${valid_url[@]}"
 do
 
         http_url=$(curl -s -i $url | head -n 1 | cut -d ' ' -f2 )
-        encodage_url=$(curl -sI $url | grep -i "^content-type" | cut -d ' ' -f3)
+        encodage_url=$(curl -sI $url | grep -i "^content-type" | cut -d ' ' -f3 | sed 's/charset=//g')
         num_url=$(lynx -dump $url | sed '/ Catégorie cachée :/,$d' | wc -w)
         
-        if 
-               [[ "$http_url" = [^45]{3} ]]
-        then
-                echo "HTTP erreur: $http_url pour $url\n Vous pouvez le corriger ou réessayer une fois." 
-	else
-		echo -e "$num:\t$url\nHTTP: $http_url"
-	        ((num++))
-	fi
-        
+       if
+               [[ "$http_url" =~ ^[45] ]];
+       then
+               sinon+=("HTTP erreur: $http_url pour $url  Vous pouvez le corriger ou réessayer une fois.")
         if
-                [[ -z "$encodage_url" ]]
-        then
-                echo "cet Url: $url n'exist pas l'encodage." 
+                        [[ -z "$encodage_url" ]];
+                then
+                        sinon+=("Cet Url: $url n'existe pas l'encodage.")
+               fi
+
         else
-                echo -e "Numbre de mots: $num_url\nL'encodage: $encodage_url" 
-	fi
+
+                echo -e "$num:\t$url\nHTTP: $http_url" 
+                ((num++))
+                echo -e "Numbre de mots: $num_url\nL'encodage: $encodage_url"  
+       fi
 done
+
+echo -e "${sinon[@]}"
 
 	       
 for url in "${invalid_url[@]}"
